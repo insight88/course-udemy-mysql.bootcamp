2919ff8554e0e3b7ff7336aa2f077ce1beb8fb91
insight88 <64120339+insight88@users.noreply.github.com>
2020-10-02 21:37:35 +0900

    0.1 udemy course: section 3,4

---
+++ create-insert.sql
@@ -0,0 +1,104 @@
+SHOW DATABASES;
+
+-- * create db
+CREATE DATABASE hello_world_db;
+
+-- * use db
+USE hello_world_db;
+
+-- * see current database
+SELECT database();
+
+-- *  dropping db
+DROP DATABASE hello_world_db;
+
+-- !  example 1
+CREATE DATABASE cat_app;
+USE cat_app;
+
+-- *  create tables
+CREATE TABLE cats
+(
+    name VARCHAR(100),
+    age INT
+);
+
+-- *  show tables
+SHOW tables;
+
+-- *  show columns
+SHOW COLUMNS FROM cats;
+DESC cats;
+
+-- *  drop table
+DROP TABLE cats;
+
+-- ! example 2
+CREATE TABLE cats
+(
+    name VARCHAR(100),
+    age INT
+);
+
+-- *  insert data
+INSERT INTO cats (name, age)
+VALUES ('Jetson', 7);
+
+-- column order doesn't matter
+INSERT INTO cats (age, name)
+VALUES (12, 'Victoria');
+
+-- * view all data in table
+SELECT * FROM cats;
+
+-- * multiple insert
+INSERT INTO cats (name, age)
+VALUES  ('Peanut', 2),
+        ('Butter', 4),
+        ('Jelly', 7);
+
+-- * view warning message
+SHOW WARNINGS;
+
+-- * NULL allowed (table cats)
+-- DESC cats;
+-- Null : Yes -> NULL data is allowed in that column
+INSERT INTO cats (name) VALUES ('Alabama');
+INSERT INTO cats () VALUES ();
+
+-- ! create new table cats 2 which doesn't allow NULL data
+CREATE TABLE cats2
+(
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+INSERT INTO cats (name) VALUES ('Texas')
+-- ? error 1364 will occur!
+
+-- * setting default values
+-- DESC : cats3;
+-- Default : unknown -> default data of that column is set as 'unknown'
+CREATE TABLE cats3
+(
+    name VARCHAR(100) DEFAULT 'unknown',
+    age INT DEFAULT 0
+);
+
+INSERT INTO cats3 (age) VALUES (13);
+INSERT INTO cats3 (name) VALUES ('Texas');
+
+-- * manually insert data with null value (explicit null)
+-- it works only when we didn't set that column as NOT NULL
+INSERT INTO cats3 (name, age) VALUES ('Montana', NULL);
+
+-- ! example 4
+CREATE TABLE cats4
+(
+    name VARCHAR(20) NOT NULL DEFAULT 'unnamed',
+    age INT NOT NULL DEFAULT 99
+);
+
+INSERT INTO cats4(name, age) VALUES('Cali', NULL);
+-- ? error 1048 occurs!
+
